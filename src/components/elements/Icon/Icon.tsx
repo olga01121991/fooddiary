51e11b451838b1d03b297eb2bd55648ea0768695
olga0112 <olga_01121991@mail.ru>
2025-04-05 11:39:55 +0300

    first commit

---
+++ src/components/elements/Icon/Icon.tsx
@@ -0,0 +1,24 @@
+//универсальная функция для иконок
+
+import { FC } from "react";
+import { TIcon } from "./iconType";
+
+interface IIconProps { //интерфейс 
+    iconType: TIcon;    //тип иконки, который прописан в отдельном файле iconType.ts (все шрифты)
+    size?: number; //размер шрифта, язательный параметр, по умолчанию 16
+} 
+
+const Icon: FC<IIconProps> = (props) => {
+    const { iconType, size = 16 } = props;
+
+    return (        
+        <i 
+            className={`icon icon-${iconType}`}
+            style={{
+                fontSize: size
+            }}
+        />
+    )
+}
+
+export default Icon;
