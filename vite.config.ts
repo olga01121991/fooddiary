08b42a764d3ad14a1fe02726a7a361802e417391
olga0112 <olga_01121991@mail.ru>
2025-04-17 18:55:23 +0300

    прописала систему цветов и создала badge

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import tsconfigPaths from 'vite-tsconfig-paths';
+// import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `@use "${path.resolve(__dirname, 'src/scss/_vars.scss')}" as *;`,
+  //     },
+  //   },
+  // },
   assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf', '**/*.eot', '**/*.svg'],
 })
